f4025f354173555a12adb39417c29994ecfae498
JoseAVallejo <osealfredovallejo25@gmail.com>
2021-07-20 18:13:41 -0500

    fix return anotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Write an asynchronous function sleep."""
+
+
 import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
-    """function
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for a random delay between 0 and max_delay seconds
 
     Args:
         max_delay (int, optional): value. Defaults to 10.
