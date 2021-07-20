653407dd91634b36156e66e00482e68770be0042
JoseAVallejo <osealfredovallejo25@gmail.com>
2021-07-20 13:37:49 -0500

    create task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Write a function concat two string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return a plus b
+
+    Args:
+        str1 (str): number one
+        str2 (str): number two
+
+    Returns:
+        str: return
+    """
+    return str1 + str2
