5ab3de05666f96de6d81bcf31aaa2c4f45f107d3
JoseAVallejo <osealfredovallejo25@gmail.com>
2021-07-20 13:20:20 -0500

    create task 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def add(numberOne: float, number_two: float) -> float:
+    """Return a plus b
+
+    Args:
+        numberOne (float): number one
+        number_two (float): number two
+
+    Returns:
+        float: return
+    """
+    return numberOne + number_two
