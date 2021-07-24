1a6f9d5f3a87b73f5e9b25883e58a2643dace081
Jose Vallejo <joseavallejo12@gmail.com>
2021-07-24 14:50:12 -0500

    fix pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Write an asynchronous function."""
+
+
 import asyncio
 from typing import Iterable
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> Iterable[float]:
     """wait and return list of time
@@ -14,7 +17,7 @@ async def wait_n(n: int, max_delay: int) -> Iterable[float]:
     Returns:
         Iterable[float]: list of time using
     """
-    time_result = [];
+    time_result = []
     for number in range(n):
         time_result.append(await wait_random(max_delay))
     return sorted(time_result)
