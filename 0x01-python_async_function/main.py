8096f9ec536af4362752378c36009eee588c4325
Jose Vallejo <joseavallejo12@gmail.com>
2021-07-24 15:38:08 -0500

    create task 3

--- 0x01-python_async_function/main.py
+++ 0x01-python_async_function/main.py
@@ -11,7 +11,17 @@ print(asyncio.run(wait_random()))
 print(asyncio.run(wait_random(5)))
 print(asyncio.run(wait_random(15))) """
 
-print('======= Test task 1 ==============')
+""" print('======= Test task 1 ==============')
 print(asyncio.run(wait_n(5, 5)))
 print(asyncio.run(wait_n(10, 7)))
-print(asyncio.run(wait_n(10, 0)))
+print(asyncio.run(wait_n(10, 0))) """
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def test(max_delay: int) -> float:
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
+
+asyncio.run(test(5))
