fa7d5cdbecdd82e2e47da60737d32b9e3952f786
Jose Vallejo <joseavallejo12@gmail.com>
2021-07-26 21:40:29 -0500

    create task 2

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""class FIFOCache that inherits from BaseCaching."""
+"""class LIFOCache that inherits from BaseCaching."""
 
 
 from typing import Union
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """Fifo class
 
     Args:
@@ -19,9 +19,11 @@ class FIFOCache(BaseCaching):
         """Add value en cache."""
         if key and item:
             new_dict = {key: item}
-            self.isFillCache()
-            new_dict.update(self.cache_data)
-            self.cache_data = new_dict
+            if self.isFillCache():
+                self.cache_data.update(new_dict)
+            else:
+                new_dict.update(self.cache_data)
+                self.cache_data = new_dict
 
     def get(self, key: str) -> Union[None, object]:
         """Get value of cache"""
@@ -32,5 +34,8 @@ class FIFOCache(BaseCaching):
     def isFillCache(self) -> None:
         """check if cache not is fill"""
         if len(self.cache_data.keys()) >= self.MAX_ITEMS:
-            delete_item = self.cache_data.popitem()[0]
-            print(f'DISCARD: {delete_item}')
+            first_element = list(self.cache_data.keys())[0]
+            self.cache_data.pop(first_element)
+            print(f'DISCARD: {first_element}')
+            return True
+        return False
