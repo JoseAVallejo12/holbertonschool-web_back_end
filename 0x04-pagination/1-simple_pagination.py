f93c14fba2d0017300f956c4f8fc68055451bc4f
Jose Vallejo <joseavallejo12@gmail.com>
2021-07-28 08:06:22 -0500

    fix get_page function

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -33,9 +33,8 @@ class Server:
         Returns:
             List[List]: list of values request
         """
-        assert(type(page) == int == type(page_size))
-        assert(page > 0 < page_size)
-        cvs_list = self.dataset()
-        index = index_range(page, page_size)
-        return [ cvs_list[item] for item in range(index[0], index[1]) if item < len(cvs_list)]
+        assert(type(page_size) == int and type(page) == int)
+        assert(page > 0 and page_size > 0)
+        beginning, end = index_range(page, page_size)
+        return self.dataset()[beginning:end]
 
