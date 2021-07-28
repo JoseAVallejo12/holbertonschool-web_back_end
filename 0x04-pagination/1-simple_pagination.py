b4ff50f74da4f2ec99c641c3afa6c1956c170a93
Jose Vallejo <joseavallejo12@gmail.com>
2021-07-27 22:31:55 -0500

    create task 2

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get specific index an a list
+
+        Args:
+            page (int, optional): [description]. Defaults to 1.
+            page_size (int, optional): [description]. Defaults to 10.
+
+        Returns:
+            List[List]: list of values request
+        """
+        assert(type(page) == int == type(page_size))
+        assert(page > 0 < page_size)
+        cvs_list = self.dataset()
+        index = index_range(page, page_size)
+        return [ cvs_list[item] for item in range(index[0], index[1]) if item < len(cvs_list)]
+
