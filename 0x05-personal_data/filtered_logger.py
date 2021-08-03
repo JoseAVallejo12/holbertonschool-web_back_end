ba237ff2e89d4faadab5c949ae82edd9b3e633ad
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-02 19:55:20 -0500

    create task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -15,3 +16,22 @@ def filter_datum(fields: List[str],
         message = re.sub(rf'{item}=.+?{separator}',
                          f'{item}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.filter_fields = fields
+        print(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """REdacting formatter class."""
+        return filter_datum(self.filter_fields,
+                            self.REDACTION,
+                            super().format(record), self.SEPARATOR)
