a4178b79a58c3a8aa7c128fd03debbde4707e5c9
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-07 14:34:25 -0500

    fix return

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -26,10 +26,11 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
-    Return:
-      - the number of each objects
+     Returns:
+        str: abort 401 status
     """
-    return abort(401,  description="Unauthorized")
+    return abort(401)
