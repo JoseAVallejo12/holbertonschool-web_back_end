0c2d1ed59cfd7af867e830487421fe3355b24f90
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-08 08:11:36 -0500

    create task 5

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -4,13 +4,14 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
 
 @app.errorhandler(404)
@@ -34,7 +35,26 @@ def forbidden(error) -> str:
     return jsonify({"error": error.description}), 403
 
 
+@app.before_request
+def before_request_func():
+    exclude_paths = ['/api/v1/status/',
+                     '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not isinstance(auth, Auth):
+        return
+    if not auth.require_auth(request.path, exclude_paths):
+        return
+
+    if (auth.authorization_header(request) is None):
+        abort(401,  description="Unauthorized")
+
+    if (auth.current_user(request) is None):
+        abort(403,  description="Forbidden")
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
+    if getenv('AUTH_TYPE') == 'auth':
+        from api.v1.auth.auth import Auth
+        auth = Auth()
     app.run(host=host, port=port, debug=True)
