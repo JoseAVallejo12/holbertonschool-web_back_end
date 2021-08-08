9fb069f14dad0ee1500360f0742606baa5541595
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-08 11:39:31 -0500

    create task 7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,15 @@
 """Auth class for the app."""
 
 from api.v1.auth.auth import Auth
+import base64
+
 
 class BasicAuth(Auth):
     """Basic Auth class."""
 
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extract_base64_authorization_header."""
+        if authorization_header is None or not isinstance(authorization_header, str) or not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.removeprefix("Basic ")
