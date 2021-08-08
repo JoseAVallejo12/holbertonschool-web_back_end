a4430e823e8a5b81062a90d5e730b027d7fe9a38
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-08 12:05:07 -0500

    fix compatibility with python 3.7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,20 +10,8 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """Extract_base64_authorization_header."""
-        if (authorization_header is None
-            or not isinstance(authorization_header, str)
-                or not authorization_header.startswith('Basic ')):
+        if authorization_header is None or not isinstance(authorization_header, str) or not authorization_header.startswith('Basic '):
             return None
 
-        return authorization_header.removeprefix("Basic").lstrip()
+        return authorization_header.split(' ')[1].lstrip()
 
-
-a = BasicAuth()
-
-print(a.extract_base64_authorization_header(None))
-print(a.extract_base64_authorization_header(89))
-print(a.extract_base64_authorization_header("Holberton School"))
-print(a.extract_base64_authorization_header("Basic Holberton"))
-print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
-print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
-print(a.extract_base64_authorization_header("Basic1234"))
