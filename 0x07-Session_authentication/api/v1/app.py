213e2f9539996b4150fbaf63e9059493f0297e89
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-14 13:20:45 -0500

    fix pycode style task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -54,7 +54,8 @@ def before_request() -> str:
     if not auth or not auth.require_auth(request.path, exclude_paths):
         return None
 
-    if (auth.authorization_header(request) is None and auth.session_cookie(request) is None):
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401,  description="Unauthorized")
 
     if (auth.current_user(request) is None):
