01a3a805243864576f6108dfabfc53fb480839fd
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-14 11:51:15 -0500

    create task 2

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 """SessionAuth class for the app."""
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """SessionAuth main class."""
+    user_id_by_session_id = {}
+
+    def __isInValid(self, parametter: str) -> bool:
+        """Validate input arg to function"""
+        if (parametter is None or not isinstance(parametter, str)):
+            return True
+        return False
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create an user id."""
+        if self.__isInValid(user_id):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id.update({
+            session_id: user_id
+        })
+        return session_id
