6b8690a224b23388a068f3035485f63de0e1d0b8
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-14 15:40:34 -0500

    fix create session

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -23,6 +23,7 @@ def Session_authentication() -> str:
     elif user[0].is_valid_password(request.form.get('password')):
         return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
-    out = jsonify(user[0].to_json())
-    out.set_cookie(getenv('SESSION_NAME'), user[0].id)
-    return out
+    session_id = auth.create_session(user[0].id)
+    response = jsonify(user[0].to_json())
+    response.set_cookie(getenv('SESSION_NAME'), session_id)
+    return response
--- 0x07-Session_authentication/request.sh
+++ 0x07-Session_authentication/request.sh
@@ -9,3 +9,5 @@ wait
 curl "http://0.0.0.0:5000/api/v1/auth_session/login" -XPOST -d "email=bobsession@hbtn.io" -d "password=fake pwd"
 wait
 curl "http://0.0.0.0:5000/api/v1/auth_session/login" -XPOST -d "email=bobsession@hbtn.io" -d "password=fake pwd" -vvv
+wait
+curl "http://0.0.0.0:5000/api/v1/users/me" --cookie "_my_session_id=df05b4e1-d117-444c-a0cc-ba0d167889c4"
