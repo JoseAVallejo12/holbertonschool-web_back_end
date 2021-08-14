2da6c5e32db7780606dbfe128cd9a237460e1e44
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-14 13:19:18 -0500

    create task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -48,13 +48,13 @@ def forbidden(error) -> str:
 @app.before_request
 def before_request() -> str:
     """Handle request."""
-    exclude_paths = ['/api/v1/status/',
+    exclude_paths = ['/api/v1/status/', '/api/v1/auth_session/login/',
                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     if not auth or not auth.require_auth(request.path, exclude_paths):
         return None
 
-    if (auth.authorization_header(request) is None):
+    if (auth.authorization_header(request) is None and auth.session_cookie(request) is None):
         abort(401,  description="Unauthorized")
 
     if (auth.current_user(request) is None):
