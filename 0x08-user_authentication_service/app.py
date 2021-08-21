dab5118e81298de11e07e8d5aa6a644de29c90ae
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-21 15:13:36 -0500

    create task 7

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,7 +3,10 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -14,5 +17,15 @@ def view_all_users() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_user():
+    user_data = request.get_json()
+    try:
+        user = AUTH.register_user(user_data['email'], user_data['password'])
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
