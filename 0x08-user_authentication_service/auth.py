773697b88ad037c659439243176fb0baea072f73
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-22 11:28:53 -0500

    change logic method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -72,8 +72,6 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """Generate reset password token."""
-        if email is None:
-            return None
         try:
             user = self._db.find_user_by(email=email)
             self._db.update_user(user.id, reset_token=_generate_uuid())
