24448f8a57bcfcaa7a7a09ff4a724c33a46627c4
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-22 11:27:55 -0500

    create task 15  Generate reset password token

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -69,3 +69,13 @@ class Auth:
         if user_id:
             self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token."""
+        if email is None:
+            return None
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, reset_token=_generate_uuid())
+        except Exception:
+            raise ValueError()
