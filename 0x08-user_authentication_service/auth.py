6f7653d0bef1c768be7084ec01cb1dd8335838e3
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-22 07:52:08 -0500

    fix encapsulation of generate uuid method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -39,7 +39,7 @@ class Auth:
             pass
         return False
 
-    def _generate_uuid(self) -> str:
+    def __generate_uuid(self) -> str:
         """Generate UUID code."""
         return str(uuid.uuid4())
 
@@ -47,7 +47,7 @@ class Auth:
         """Create session ID if user exist in DB."""
         try:
             user = self._db.find_user_by(email=email)
-            session_id = self._generate_uuid()
+            session_id = self.__generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
         except Exception:
