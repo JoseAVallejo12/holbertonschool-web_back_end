ef214d69dc6261b3a380ceda9662da0c2d4886e0
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-22 13:47:16 -0500

    more fix....

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -79,17 +79,17 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route("/reset_password", methods=['PUT'])
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
-    """Reset password token."""
+    """ Update password end-point """
     email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
+    token = request.form.get('reset_token')
+    password = request.form.get('new_password')
     try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
+        AUTH.update_password(token, password)
     except Exception:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
