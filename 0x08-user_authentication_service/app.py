80398de1f0135b96ff04a400b52cf943d20304b5
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-22 12:49:28 -0500

    more fix...

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -81,7 +81,7 @@ def get_reset_password_token() -> Response:
 
 
 @app.route("/reset_password", methods=['PUT'], strict_slashes=False)
-def update_password() -> Response:
+def update_password():
     """Reset password token."""
     email = request.form.get('email')
     token = request.form.get('reset_token')
