edc640c5a0b8fe9786043e2028425afbc67a174c
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-22 13:14:13 -0500

    create schema task 12

--- 0x08-user_authentication_service/main.py
+++ 0x08-user_authentication_service/main.py
@@ -4,22 +4,45 @@ Main file
 """
 from auth import Auth
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-password_new = 'MyPwdOfBobujlk'
-auth = Auth()
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+AUTH = Auth()
 
-auth.register_user(email, password)
-id_session = auth.create_session(email)
-reset_token_pwd = auth.get_reset_password_token(email=email)
-try:
-    auth.update_password(reset_token=reset_token_pwd, password=password_new)
-except Exception as e:
-    print('no fue posible')
-print(f'session id: {id_session}')
-print(f'reset_token_pwd: {reset_token_pwd}')
-# print(auth.get_user_from_session_id(auth.create_session(email)))
-# print(auth.get_user_from_session_id(auth.create_session('dd')))
-# auth.get_reset_password_token(email)
-# print(auth.create_session("unknown@email.com"))
-# auth.update_password(reset_token="32h32i3u", password="11232")
+def register_user(email: str, password: str) -> None:
+    assert(True)
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    assert(True)
+
+def log_in(email: str, password: str) -> str:
+    assert(True)
+
+def profile_unlogged() -> None:
+    assert(True)
+
+def profile_logged(session_id: str) -> None:
+    assert(True)
+
+def log_out(session_id: str) -> None:
+    assert(True)
+
+def reset_password_token(email: str) -> str:
+    assert(True)
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    assert(True)
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
