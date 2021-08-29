0721dc1b21543546e1e0d378c0dfb6c22c8fe910
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-29 08:18:15 -0500

    set only requirements test

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,14 +2,13 @@
 """utils testing class"""
 from unittest import TestCase
 from utils import access_nested_map, memoize
-from parameterized import parameterized, parameterized_class
+from parameterized import parameterized
 
 
 class TestAccessNestedMap(TestCase):
     """Main testing class."""
 
     @parameterized.expand([
-        ({"a": {"b": {"c": 1}}}, ["a", "b", "c"], 1),
         ({"a": 1}, ['a'], 1),
         ({"a": {"b": 2}}, ['a'], {"b": 2}),
         ({"a": {"b": 2}}, ['a', 'b'], 2)
