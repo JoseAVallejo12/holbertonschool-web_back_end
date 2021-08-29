b74bd30dcef5b57b28a3fa5867a34c775e1f188f
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-29 09:01:38 -0500

    create task 2

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """utils testing class"""
 from unittest import TestCase
-from utils import access_nested_map, memoize
+from unittest.mock import patch
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(TestCase):
-    """Main testing class."""
+    """main access_nested_map testing."""
 
     @parameterized.expand([
         ({"a": 1}, ['a'], 1),
@@ -25,3 +26,19 @@ class TestAccessNestedMap(TestCase):
         """Test raise error method access_nested_map."""
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
+
+class TestGetJson(TestCase):
+    """ Class for Get Json Tests """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Test that utils.get_json returns the expected result."""
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
