6246bdeb6301c2cae88ade8d4fb4ae3d83a36bac
Jose Vallejo <joseavallejo12@gmail.com>
2021-08-29 08:30:29 -0500

    create task 1

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,12 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """method access_nested_map test."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ['a']),
+        ({"a": 1}, ['a', 'b'])
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test raise error method access_nested_map."""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
