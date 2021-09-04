e2f8f9a8eb5c3328d123f2e8ee2273b55afa1487
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-04 10:02:37 -0500

    add comment to script

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,9 @@
+-- create database holberton and table user
+
 CREATE TABLE IF NOT EXISTS `users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `name` VARCHAR(255),
-  `country` ENUM('US', 'CO', 'TN') DEFAULT 1
+  `country` ENUM('US', 'CO', 'TN') DEFAULT 1,
   PRIMARY KEY (`id`)
 );
