28486bfeab87aba9b59b6663244a86580d57523f
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-04 09:50:01 -0500

    more fix

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS `holberton`.`users` (
+-- create database holberton and table user
+
+CREATE TABLE IF NOT EXISTS `users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `name` VARCHAR(255),
