f351e8269982c081bf1dac79aa2d919e1afba765
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-04 09:39:05 -0500

    fix schema

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -4,8 +4,8 @@ CHARACTER SET `utf8`
 COLLATE `utf8_persian_ci`;
 
 CREATE TABLE IF NOT EXISTS `holberton`.`users` (
-  `id` INT(12) NULL AUTO_INCREMENT,
-  `email` VARCHAR(255) NOT NULL UNIQUE
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
   `name` VARCHAR(255),
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARSET = utf8 COLLATE utf8_persian_ci;
