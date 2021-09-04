71b710950e760c8b58631fabdab1384f6cced06d
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-04 09:45:20 -0500

    delete database create

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,6 @@
-DROP DATABASE IF EXISTS `holberton`;
-CREATE DATABASE IF NOT EXISTS `holberton`
-CHARACTER SET `utf8`
-COLLATE `utf8_persian_ci`;
-
 CREATE TABLE IF NOT EXISTS `holberton`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `name` VARCHAR(255),
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB CHARSET = utf8 COLLATE utf8_persian_ci;
+);
