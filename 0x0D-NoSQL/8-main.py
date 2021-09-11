e9af1b91da3d80226f5cff35c05c88391bdda75e
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-11 13:00:53 -0500

    create task 9

--- 0x0D-NoSQL/8-main.py
+++ 0x0D-NoSQL/8-main.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 """ 8-main """
 from pymongo import MongoClient
-from os import environ
 list_all = __import__('8-all').list_all
-
-USER_MONGO = environ['USER_MONGO']
-PASSWORD = environ['PASSWORD']
-DB_MONGO = environ['DB_MONGO']
-STRING_CONNECTION = F'mongodb+srv://{USER_MONGO}:{PASSWORD}@cluster0.idafl.mongodb.net/{DB_MONGO}?retryWrites=true&w=majority'
+mongo_client = __import__('mongo_connect').mongo_client
 
 if __name__ == "__main__":
-    client = MongoClient(STRING_CONNECTION)
+    client = mongo_client()
     school_collection = client.Holberton.school
     schools = list_all(school_collection)
     print(school_collection.find_one())
