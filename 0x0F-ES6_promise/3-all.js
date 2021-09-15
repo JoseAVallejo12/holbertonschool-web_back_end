e1fdf5606d8b002220317b2869e7a5331edf5f8d
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-14 21:03:41 -0500

    use Promise.all static method

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,7 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto()
-    .then(({ body }) => body)
-    .then((body) => createUser().then(({ firstName, lastName }) => console.log(`${body} ${firstName} ${lastName}`)));
+  Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    const { body } = values[0];
+    const { firstName, lastName } = values[1];
+    console.log(`${body} ${firstName} ${lastName}`);
+  });
 }
