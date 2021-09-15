710821b930c633c1f24b24f08a3b70a8d2b5b3bb
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-14 22:18:04 -0500

    fix output task 6

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -6,5 +6,16 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ])
-    .then((values) => values);
+    .then((values) => values.map((data) => {
+      if (data.reason) {
+        return {
+          status: data.status,
+          value: data.reason.message,
+        };
+      }
+      return {
+        status: data.status,
+        value: `${data.value.firstName} ${data.value.lastName}`,
+      };
+    }));
 }
