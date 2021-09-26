9736f038fd548472d9fed8feaa8f276e0ae7f588
Jose Vallejo <joseavallejo12@gmail.com>
2021-09-26 15:12:29 -0500

    improve task 8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,12 @@
 export default function cleanSet(set, startString) {
-  set.forEach((element) => {
-    element.startsWith(startString);
-  });
+  let res = '';
+  if (startString) {
+    set.forEach((element) => {
+      const rexString = new RegExp(`^${startString}`);
+      if (element.match(rexString)) {
+        res += `${element.slice(startString.length, element[-1])}-`;
+      }
+    });
+  }
+  return res.slice(0, res.length - 1);
 }
