ca7f733d3f02ffc9e4dc56a5a0d1f2d3f692486d
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2021-10-02 11:39:37 -0500

    fix eslint config file and fix calculatenumber calc

--- 0x13-unittests_in_js/.eslintrc.js
+++ 0x13-unittests_in_js/.eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
+    mocha: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/all',
   ],
   globals: {
     Atomics: 'readonly',
--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,33 +1,20 @@
 const asserts = require('assert');
 const calculateNumber = require('./0-calcul');
 
-// eslint-disable-next-line no-undef
-describe('sum func', () => {
-  // eslint-disable-next-line no-undef
-  it('result should be 4', () => {
-    const result = Math.round(calculateNumber(1, 3));
-
-    asserts.equal(result, 4);
+describe('calculateNumber', function () {
+  it('result should be 4', function () {
+    asserts.equal(calculateNumber(1, 3), 4);
   });
 
-  // eslint-disable-next-line no-undef
   it('result should be 5', () => {
-    const result = Math.round(calculateNumber(1, 3.7));
-
-    asserts.equal(result, 5);
+    asserts.equal(calculateNumber(1, 3.7), 5);
   });
 
-  // eslint-disable-next-line no-undef
   it('result should be 5', () => {
-    const result = Math.round(calculateNumber(1.2, 3.7));
-
-    asserts.equal(result, 5);
+    asserts.equal(calculateNumber(1.2, 3.7), 5);
   });
 
-  // eslint-disable-next-line no-undef
   it('result should be 6', () => {
-    const result = Math.round(calculateNumber(1.5, 3.7));
-
-    asserts.equal(result, 5);
+    asserts.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
