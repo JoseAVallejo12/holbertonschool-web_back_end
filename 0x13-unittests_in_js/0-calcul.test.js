af419be78e6093d4ebb5bf53d0499d528caf3906
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2021-10-02 10:37:03 -0500

    add slint and allow it linter

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,33 @@
+const asserts = require('assert');
 const calculateNumber = require('./0-calcul');
-const asserts = require('assert').strict;
 
+// eslint-disable-next-line no-undef
 describe('sum func', () => {
+  // eslint-disable-next-line no-undef
   it('result should be 4', () => {
-    asserts.equal(calculateNumber(1, 3), 4)
+    const result = Math.round(calculateNumber(1, 3));
+
+    asserts.equal(result, 4);
   });
+
+  // eslint-disable-next-line no-undef
   it('result should be 5', () => {
-    asserts.equal(calculateNumber(1, 3.7), 5)
+    const result = Math.round(calculateNumber(1, 3.7));
+
+    asserts.equal(result, 5);
   });
+
+  // eslint-disable-next-line no-undef
   it('result should be 5', () => {
-    asserts.equal(calculateNumber(1.2, 3.7), 5)
+    const result = Math.round(calculateNumber(1.2, 3.7));
+
+    asserts.equal(result, 5);
   });
+
+  // eslint-disable-next-line no-undef
   it('result should be 6', () => {
-    asserts.equal(calculateNumber(1.5, 3.7), 5)
+    const result = Math.round(calculateNumber(1.5, 3.7));
+
+    asserts.equal(result, 5);
   });
-})
+});
